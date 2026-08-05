50ee027c346eb90a880aaba1f044bf28d386e605
Evgenia Kostenko <evgeniakostenko@mac.mynetworksettings.com>
2026-08-05 19:23:20 -0400

    Update website

--- src/components/requirements-section.tsx
+++ src/components/requirements-section.tsx
@@ -14,7 +14,7 @@ const payItems = [
     title: "Minimum 3 Months of Experience",
     icon: <img src="/road.svg" alt="Experience" className="w-8 h-8" />,
     description:
-      "At least 3 months of verifiable OTR driving experience is required to join our team.",
+      "At least 6 months of verifiable OTR driving experience is required to join our team.",
   },
   {
     title: "Pass DOT Requirements",
